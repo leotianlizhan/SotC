7cfd1b75eeb9ca99f8ff15022d9ea1ba294dd9d6
Leo Zhan <leotianlizhan@gmail.com>
2017-12-17 18:48:48 -0500

    Use vuetify instead. It's a material css framework

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
 
-Vue.config.productionTip = false
+Vue.use(Vuetify)
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  render: h => h(App)
 })
