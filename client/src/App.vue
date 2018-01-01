0c2b164eb2d25559d194df98b3d116625bb239aa
Leo Zhan <leotianlizhan@gmail.com>
2017-12-31 19:15:08 -0500

    This way of adding vuetify seem better

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,52 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-side-icon 
+        @click="sideNav = !sideNav"
+        class="hidden-sm-and-up"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>State of the Contract</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer
+      v-model="sideNav"
+      fixed
+    >
+      <v-list>
+        <v-list-tile v-for="item in menuItems" :key="item.title">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <main>
+      <router-view>
+      </router-view>
+    </main>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    data () {
+      return {
+        sideNav: false,
+        menuItems: [
+          {icon: 'event', title: 'Events'},
+          {icon: 'room', title: 'Read Contract'},
+          {icon: 'person', title: 'ABC'},
+          {icon: 'face', title: 'DEF'},
+          {icon: 'lock_open', title: 'Huh'}
+        ]
+      }
+    }
+  }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
+import('../node_modules/vuetify/dist/vuetify.min.css') // Ensure you are using css-loader
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
